5a4c7bdaa87a8ab164987245750311b15b6977ad
Seori-Seori <bsp0301@gmail.com>
2026-09-09 22:50:49 +0900

    캐릭터 생성까지 완료.

---
+++ All_InOne/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace All_InOne
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.직업 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // 직업
+            // 
+            this.직업.FormattingEnabled = true;
+            this.직업.Items.AddRange(new object[] {
+            "전사",
+            "법사",
+            "궁수",
+            "용사",
+            "백수"});
+            this.직업.Location = new System.Drawing.Point(283, 99);
+            this.직업.Name = "직업";
+            this.직업.Size = new System.Drawing.Size(121, 23);
+            this.직업.TabIndex = 1;
+            this.직업.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.textBox1.Location = new System.Drawing.Point(283, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(183, 25);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(145, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "이름을 입력하세요";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(145, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "직업을 선택하세요";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(130, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "난이도를 선택하세요";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(283, 287);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "HP :100";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(257, 253);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "캐릭터 생성";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(283, 151);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(58, 19);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "쉬움";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(283, 171);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(58, 19);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "중간";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(283, 196);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(73, 19);
+            this.radioButton3.TabIndex = 11;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "어려움";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(351, 253);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "클릭하세요";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(283, 302);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 15);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "공격력 : 20";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.직업);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox 직업;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ All_InOne/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace All_InOne
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        int ghp;
+        int gattack;
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+   
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           Text = "이름을 입력하세요";
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("이름을 입력하세요");
+                return;
+            }
+            if (직업.SelectedItem == null)
+            {
+                MessageBox.Show("직업을 선택하세요");
+                return;
+            }
+            if(radioButton1.Checked == false && radioButton2.Checked == false && radioButton3.Checked == false)
+            {
+                MessageBox.Show("난이도를 선택하세요");
+                return;
+            }
+
+            int hp;
+            int attack;
+            if (직업.SelectedItem.ToString() == "전사")
+            {
+                hp = 1000;
+                attack = 20;
+                ghp = hp;
+                gattack = attack;
+            }
+            else if (직업.SelectedItem.ToString() == "법사")
+            {
+                hp = 100;
+                attack = 525;
+                ghp = hp;
+                gattack = attack;
+            }
+            else if (직업.SelectedItem.ToString() == "궁수")
+            {
+                hp = 400;
+                attack = 15;
+                ghp = hp;
+                gattack = attack;
+            }
+
+            else if (직업.SelectedItem.ToString() == "용사")
+            {
+                hp = 2000;
+                attack = 34440;
+                ghp = hp;
+                gattack = attack;
+            }
+            else if (직업.SelectedItem.ToString() == "백수")
+            {
+                hp = 50;
+                attack = 5;
+                ghp = hp;
+                gattack = attack;
+            }
+            int finalHp = ghp;
+            int finalAttack = gattack;
+
+            if (radioButton1.Checked)
+            {
+                finalHp = ghp + 2999;
+                finalAttack = gattack + 20;
+                label4.Text = "hp: " + finalHp;
+                label6.Text = "공격력: " + finalAttack;
+            }
+            else if (radioButton2.Checked)
+            {
+                finalHp = ghp + 100;
+                finalAttack = gattack + 25;
+                label4.Text = "hp: " + finalHp;
+                label6.Text = "공격력: " + finalAttack;
+            }
+
+
+            else if (radioButton3.Checked)
+            {
+                finalHp = ghp - 600;
+                finalAttack = gattack - 10;
+                label4.Text = "hp: " + finalHp;
+                label6.Text = "공격력: " + finalAttack;
+            }
+
+            string name = textBox1.Text;
+            string job = 직업.SelectedItem.ToString();
+
+            string dyp = "";
+            if(radioButton1.Checked)
+            {
+                dyp = "쉬움";
+            }
+            else if (radioButton2.Checked)
+            {
+                dyp = "중간";
+            }
+            else if (radioButton3.Checked)
+            {
+                dyp = "어려움";
+            }
+            MessageBox.Show("이름: " + name + "\n직업: " + job + "\n난이도: " + dyp + "\n최종 HP: " + finalHp + "\n최종 공격력: " + finalAttack, "캐릭터 생성 완료!");
+
+
+        } 
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
