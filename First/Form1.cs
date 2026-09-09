3a1427f0f2d3e0597c0c37211913aedc3aa56839
qatjfl <bspark0211@naver.com>
2026-09-09 19:45:58 +0900

    Add initial C# practice projects

---
+++ First/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection.Emit;
+using System.Windows.Forms;
+
+namespace First
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "안녕하세요, 서리님!";
+        }
+    }
+}
---
+++ pj2_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pj2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            int result;
+
+            string str;
+
+            Console.Write("첫번쨰 계산할 값을 입력하세요 =>");
+            str = Console.ReadLine();
+            a = Convert.ToInt32(str);
+
+            Console.Write("두번째 계산할 값을 입력하세요 =>");
+            str = Console.ReadLine();
+            b = Convert.ToInt32(str);
+
+
+            result = a + b;
+            Console.WriteLine(a + "+" + b + "=" + result);
+
+            result = a - b;
+            Console.WriteLine(a + "-" + b + "=" + result);
+
+            result = a * b;
+            Console.WriteLine(a + "*" + b + "=" + result);
+
+            result = a / b;
+            Console.WriteLine(a + "/" + b + "=" + result);
+
+
+        }
+    }
+}
