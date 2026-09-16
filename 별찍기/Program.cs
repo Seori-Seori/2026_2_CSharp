2f9233a57af929178b1322b754047668e960143b
qatjfl <bspark0211@naver.com>
2026-09-16 17:17:14 +0900

    다이아몬드 찍기.

---
+++ 별찍기/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 별찍기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, k;
+
+            i = 0;
+            while (i < 9)
+            {
+                if (i < 5)
+                {
+                    k = 0;
+                    while (k < 4 - i)
+                    {
+                        Console.Write(" ");
+                        k += 1;
+                    }
+                    k = 0;
+                    while (k < 2 * i + 1)
+                    {
+                        Console.Write('\u2605');
+                        k += 1;
+                    }
+                }
+                else
+                {
+                    k = 0;
+                    while (k < i - 4)
+                    {
+                        Console.Write(" ");
+                        k += 1;
+                    }
+                    k = 0;
+                    while (k < 2 * (8 - i) + 1)
+                    {
+                        Console.Write('\u2605');
+                        k += 1;
+                    }
+                }
+                Console.WriteLine();
+                i += 1;
+            }
+        }
+    }
+}
