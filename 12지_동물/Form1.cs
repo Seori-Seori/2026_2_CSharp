1d72900b20f7e686c97725dbbc93fdb5daf7a0e5
qatjfl <bspark0211@naver.com>
2026-09-16 16:52:18 +0900

    12지 동물 띠 완성

---
+++ 12지_동물/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _12지_동물
+{
+    public partial class ｒｕｎ : Form
+    {
+        public ｒｕｎ()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         this.Text = "12지 동물 띠 알아보기";
+
+        int yesr = int.Parse(textBox1.Text);
+            string imageFile = "C:\\CookC#\\images\\";
+
+            switch (yesr % 12)
+            {
+                case 0:
+                    imageFile += "원숭이.png";
+                    break;
+                case 1:
+                    imageFile += "닭.png";
+                    break;
+                case 2:
+                    imageFile += "개.png";
+                    break;
+                case 3:
+                    imageFile += "돼지.png";
+                    break;
+                case 4:
+                    imageFile += "쥐.png";
+                    break;
+                case 5:
+                    imageFile += "소.png";
+                    break;
+                case 6:
+                    imageFile += "호랑이.png";
+                    break;
+                case 7:
+                    imageFile += "토끼.png";
+                    break;
+                case 8:
+                    imageFile += "용.png";
+                    break;
+                case 9:
+                    imageFile += "뱀.png";
+                    break;
+                case 10:
+                    imageFile += "말.png";
+                    break;
+                case 11:
+                    imageFile += "양.png";
+                    break;
+            }
+
+            pictureBox1.Image = Image.FromFile(imageFile);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Image = Image.FromFile(imageFile);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+        }
+    }
+}
---
+++ 학점콘솔/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 학점콘솔
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int score;
+
+            Random rnd = new Random();
+
+            for( int i =0; i < 5; i++)
+            {
+                score = rnd.Next(0, 101);
+                Console.WriteLine("취득 점수" + score+ "==>");
+                if (score >= 90)
+                {
+                    Console.WriteLine("학점: A");
+                }
+                else if (score >= 80)
+                {
+                    Console.WriteLine("학점: B");
+                }
+                else if (score >= 70)
+                {
+                    Console.WriteLine("학점: C");
+                }
+                else if (score >= 60)
+                {
+                    Console.WriteLine("학점: D");
+                }
+                else
+                {
+                    Console.WriteLine("학점: F");
+                }
+            }
+        }
+    }
+}
